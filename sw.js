73e2e8659e7befb64bc47c1eddaa6d51fb32eaa5
jppegarare <jppegarare18@gmail.com>
2024-10-25 17:32:03 -0400

    adicionando funcionalidade offline

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@ self.addEventListener("install", (e) => {
 });
 
 self.addEventListener("fetch", (e) => {
-    e.respondWidth(
+    e.respondWith(
         caches.match(e.request).then((response) => {
             return response || fetch(e.request);
         })
